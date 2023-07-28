ebdbd45e4fb826eeb7b8d588f5943397d62a2ac1
Noah Bosshart <noahbosshart@gmail.com>
2023-07-28 08:51:48 +0200

    adding example plot

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #### Helper Functions in R ####
 
-1. socials_caption: Automatically adds socials to your ggplot2 caption (supported socials: twitter, linkedin, github)
+1. socials_caption: Automatically adds socials to your ggplot2 subtitle or caption (supported socials: twitter, linkedin, github)
+![Example](plots/captions_example.png)
--- socials_caption.R
+++ socials_caption.R
@@ -1,7 +1,8 @@
 # Loading Packages --------------------------------------------------------
 library(ggtext)
-library(ggplot2)
+library(tidyverse)
 library(fontawesome)
+library(palmerpenguins)
 
 # Loading Fonts -----------------------------------------------------------
 
@@ -9,11 +10,15 @@ library(fontawesome)
 sysfonts::font_add(family = "Font Awesome 6 Brands",
                    regular = "FONTS/Font Awesome 6 Brands-Regular-400.otf")
 showtext::showtext_auto()
+showtext::showtext_opts(dpi=300)
 
+# Color palette -----------------------------------------------------------
 noah_green <- c("#58d68d","#2ecc71","#28b463","#239b56","#1d8348","#186a3b")
 noah_yellow <- c("#f4d03f","#f1c40f","#d4ac0d","#b7950b","#9a7d0a","#7d6608")
 noah_gray <- c("#cacfd2", "#bdc3c7", "#a6acaf", "#909497", "#797d7f", "#626567")
 
+
+# Function ----------------------------------------------------------------
 socials_caption <- function(icon_color = noah_green[6], font_color = noah_gray[6], twitter = "@noahbosshart", linkedin = "noahbosshart", github = "noboss93"){
   # adding social icons
   twitter_icon <- "&#xf099"
@@ -31,3 +36,17 @@ socials_caption <- function(icon_color = noah_green[6], font_color = noah_gray[6
 }
 print("*** Function loaded ***", quote = FALSE)
 print("use text size = 10 and margin(t = 5)", quote = FALSE)
+
+
+# Example -----------------------------------------------------------------
+palmerpenguins::penguins %>%
+  ggplot(aes(x = flipper_length_mm, y = body_mass_g, color = island, shape = island))+
+  geom_point()+
+  scale_color_manual(values = c(noah_green[2], noah_yellow[2], noah_gray[5]))+
+  labs(title = "Add socials as subtitles or as captions", caption = socials_caption(), subtitle = socials_caption())+
+  theme(plot.caption = element_textbox_simple(size = 10, margin = margin(t = 5)),
+        plot.subtitle = element_textbox_simple(size = 10, margin = margin(b = 5)),
+        legend.position = c(0.2,0.8))
+
+# ggsave("plots/captions_example.png", width  = 150, height  = 150, units = "mm")
+
